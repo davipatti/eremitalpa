8701b4d4567e530d55360fb7980051b775d07f30
David Pattinson <djp65@cam.ac.uk>
2020-01-03 13:21:27 +0100

    Fixed script for plotting labelled tree.

--- bin/plot-labelled-phylogeny.py
+++ bin/plot-labelled-phylogeny.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-import eremitalpa
-import matplotlib.pyplot as plt
-import dendropy
-import sys
 import argparse
+import sys
+
+import dendropy
+import matplotlib.pyplot as plt
+
+import eremitalpa as ere
+
 parser = argparse.ArgumentParser(description="Plot a labelled phylogeny")
 parser.add_argument("--newick", help="Path to newick tree")
 parser.add_argument("--filename", help="Path of filename to save")
@@ -11,15 +14,12 @@ args = parser.parse_args()
 tree = dendropy.Tree.get(path=args.newick, schema="newick")
 tree.ladderize()
 # Compute a good figure size
-tree = eremitalpa.compute_tree_layout(tree)
+tree = ere.compute_tree_layout(tree)
 n_leaves = len(tuple(tree.leaf_node_iter()))
 height = n_leaves / 50.0
 width = 10
 # Plot
 fig, ax = plt.subplots(figsize=(width, height))
-eremitalpa.plot_dendropy_tree(tree,
-                              labels=True,
-                              label_kws=dict(fontsize=1),
-                              leaf_kws=dict(s=0),
-                              compute_layout=False)
+ere.plot_tree(tree, labels=True, label_kws=dict(fontsize=1),
+              leaf_kws=dict(s=0), compute_layout=False)
 plt.savefig(args.filename, bbox_inches="tight")
